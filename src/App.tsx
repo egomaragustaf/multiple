781bbcaae158a3158b2ebc4b8db7af9482351bde
Ego Maragustaf <egomaragustaf@gmail.com>
2024-10-16 01:34:47 +0700

    init react vite

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Box, Button, Grid2 } from "@mui/material";
+
+export default function App() {
+  return (
+    <Box border={1} width={400} padding={4}>
+      <Grid2 container spacing={2}>
+        <Grid2 container spacing={2}>
+          <Button variant="contained">Hello</Button>
+          <Button variant="outlined">Hello</Button>
+          <Button variant="outlined">Hello</Button>
+          <Button variant="contained">Hello</Button>
+          <Button variant="contained">Hello</Button>
+        </Grid2>
+        <Button variant="outlined">Hello</Button>
+        <Button variant="contained">Hello</Button>
+        <Button variant="contained">Hello</Button>
+      </Grid2>
+    </Box>
+  );
+}
